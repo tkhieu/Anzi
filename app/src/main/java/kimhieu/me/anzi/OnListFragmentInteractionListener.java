028506cb10b50df9533817f1fde3a7614a735387
Hieu Tran <tr.kimhieu@gmail.com>
2016-06-28 20:31:22 +0700

    Add two fragment

---
+++ app/src/main/java/kimhieu/me/anzi/OnListFragmentInteractionListener.java
@@ -0,0 +1,10 @@
+package kimhieu.me.anzi;
+
+import kimhieu.me.anzi.dummy.DummyContent;
+
+/**
+ * Created by kimhieu on 6/28/16.
+ */
+public interface OnListFragmentInteractionListener {
+    void onListFragmentInteraction(DummyContent.DummyItem item);
+}
--- app/src/main/java/kimhieu/me/anzi/SearchActivity.java
+++ app/src/main/java/kimhieu/me/anzi/SearchActivity.java
@@ -1,5 +1,7 @@
 package kimhieu.me.anzi;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -33,6 +36,18 @@ public class SearchActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+
+    private SearchView.OnQueryTextListener mQueryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +83,11 @@ public class SearchActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
@@ -135,24 +155,28 @@ public class SearchActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return GooglePlaceResultFragment.newInstance(1);
+                case 1:
+                    return FoursquareResultFragment.newInstance(1);
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "GOOGLE PLACE";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "FOURSQUARE";
             }
             return null;
         }
