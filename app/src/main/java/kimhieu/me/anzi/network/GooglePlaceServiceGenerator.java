f3da5cab15fd16c9f417500e05ebd8d725f477a2
Hieu Tran <tr.kimhieu@gmail.com>
2016-06-28 19:24:32 +0700

    Setup Retrofit, Create GooglePlace and 4square service

---
+++ app/src/main/java/kimhieu/me/anzi/network/GooglePlaceServiceGenerator.java
@@ -0,0 +1,34 @@
+package kimhieu.me.anzi.network;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class GooglePlaceServiceGenerator {
+
+    public static final String API_BASE_URL = "https://maps.googleapis.com/maps/api/place/";
+    private static HttpLoggingInterceptor logging;
+    private static OkHttpClient.Builder httpClientBuilder;
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        httpClientBuilder = new OkHttpClient.Builder();
+        logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClientBuilder.addInterceptor(logging);
+        httpClientBuilder
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS);
+        Retrofit retrofit = builder.client(httpClientBuilder.build()).build();
+        return retrofit.create(serviceClass);
+    }
+}
