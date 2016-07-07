6267aeb9cf50a5b6ae0e1aed80ceec3c4e9f591f
Hieu Tran <tr.kimhieu@gmail.com>
2016-07-07 20:00:33 +0700

    Get current location use Google Play Service

--- app/src/main/java/kimhieu/me/anzi/MapsActivity.java
+++ app/src/main/java/kimhieu/me/anzi/MapsActivity.java
@@ -1,8 +1,17 @@
 package kimhieu.me.anzi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,15 +27,72 @@ import kimhieu.me.anzi.models.foursquare.Venue;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     private GoogleMap mMap;
     private List<Venue> list = new ArrayList<>();
-
+    private GoogleApiClient mGoogleApiClient;
+    private android.location.Location mLastLocation;
+    private double mLatitude;
+    private double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         list = getIntent().getParcelableArrayListExtra("list");
         setContentView(R.layout.activity_maps);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        Log.d(TAG, "Service Connected");
+                        if (ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // Should we show an explanation?
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this,
+                                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                                // Show an expanation to the user *asynchronously* -- don't block
+                                // this thread waiting for the user's response! After the user
+                                // sees the explanation, try again to request the permission.
+
+                            } else {
+
+                                // No explanation needed, we can request the permission.
+
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                                // app-defined int constant. The callback method gets the
+                                // result of the request.
+                            }
+                        }
+                        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                                mGoogleApiClient);
+                        if (mLastLocation != null) {
+                            mLatitude = mLastLocation.getLatitude();
+                            mLongitude = mLastLocation.getLongitude();
+                            Log.d(TAG, String.valueOf(mLatitude));
+                            Log.d(TAG, String.valueOf(mLongitude));
+                        }
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.d(TAG, "Connection Suspended");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.d(TAG, "Connection Failed");
+                    }
+                })
+                .addApi(LocationServices.API)
+                .build();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,8 +119,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16));
         for (Venue v : list) {
             Location l = v.getLocation();
-            LatLng latLng = new LatLng(l.getLat(),l.getLng());
+            LatLng latLng = new LatLng(l.getLat(), l.getLng());
             mMap.addMarker(new MarkerOptions().position(latLng).title(v.getName()));
         }
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
